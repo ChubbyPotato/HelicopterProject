050a1d8eeef4548a207b6fef4525de84684c9534
Suhan Gui <suhangui@hanovernorwichschools.org>
2016-03-30 01:41:24 +0800

    Updated from Brython Server: 3/29/2016 1:41:24 PM

--- Solver.py
+++ Solver.py
@@ -1,8 +1,12 @@
 from math import sin, cos, tan, sqrt, asin, radians, degrees,acos,atan
 
+print("Group: Suhan Gui, Joseph Goff, Matthew Siegel\n")
+
 Vert=float(input("Measure of Vertical Angle (E): "))
 Aye=float(input("Measure of Azimuth Angle (A): "))
 Z=float(input("Height (Z): "))
+XL=float(input("X coordinate of the antenna: "))
+YL=float(input("Y coordinate of the antenna: "))
 
 E1= 90-Vert
 A=radians(Aye) #conversion
@@ -13,8 +17,8 @@ OP=Z/tan(E) #O is the orgin OP is distance between heli and person
 x=OP*cos(A)
 y=OP*sin(A)
 
-x1=x-40.6
-y1=y+106
+x1=x-XL
+y1=y-YL
 
 D=x1*x1+y1*y1
 bd=sqrt(D)
@@ -26,7 +30,7 @@ ele=atan(Rcord/bd)
 recon=degrees(ele)
 NA=90-recon
 
-xdist=x-40.6
+xdist=x-XL
 Az=acos(xdist/bd)
 As=degrees(Az)
 Duh=Aye%360
